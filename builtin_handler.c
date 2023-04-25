de72a305c37d025ff4716af7a1f543a5e6048496
Tonia-ram <tonia.ramphisa@gmail.com>
2023-04-25 13:41:36 -0700

    setenv and unsetenv

--- builtin_handler.c
+++ builtin_handler.c
@@ -1,5 +1,22 @@
 #include "main.h"
 
+int setenv_var(const char* name, const char* value)
+{
+    int status = setenv(name, value, 1);
+    if (status != 0) {
+        perror("setenv");
+    }
+    return status;
+}
+
+int unsetenv_var(const char* name) {
+    int status = unsetenv(name);
+    if (status != 0) {
+        perror("unsetenv");
+    }
+    return status;
+}
+
 int is_builtin(char **command_array)
 {
 	if (command_array == NULL)
@@ -11,8 +28,12 @@ int is_builtin(char **command_array)
 		return (1);
 	else if (strcmp(command_array[0], "env") == 0)
 		return (1);
+	else if (strcmp(command_array[0], "setenv") == 0)
+		return (1);
+	else if (strcmp(command_array[0], "unsetenv") == 0)
+		return (1);
 
-	return (0);	
+	return (0);
 }
 
 int builin_handler(char **command_array, char **envp)
@@ -41,7 +62,17 @@ int builin_handler(char **command_array, char **envp)
 		}
 		return (0);
 	}
-
-
+	else if (strcmp(command_array[0], "setenv") == 0)
+	{
+		if (command_array[1] == NULL || command_array[2] == NULL)
+       			return (0);
+		setenv_var(command_array[1], command_array[2]);
+	}
+	else if (strcmp(command_array[0], "unsetenv") == 0)
+	{
+	if (command_array[1] == NULL)
+		return (0);
+	unsetenv_var(command_array[1]);
+	}
 	return (1);
 }
